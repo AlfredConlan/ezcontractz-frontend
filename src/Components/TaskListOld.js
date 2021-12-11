72fdb52a508027b8bc5e38ba802b286bf9997d8f
Al Conlan <oconellan@gmail.com>
2021-12-11 15:21:00 -0600

    working on registration

---
+++ src/Components/TaskListOld.js
@@ -0,0 +1,18 @@
+// import Profile from "./Profile";
+// import BasicTable from './dataTable'
+import TaskTable from "./TasksTable";
+// import SetUserName from "./SetUserName";
+
+export default function TaskList() {
+  return (
+    <div>
+      {/* <SetUserName /> */}
+      <div className="profileHouseSection">
+        <h1 className="text-center p-4 text-primary">Your Tasks</h1>
+      </div>
+      <div className="taskList">
+        <TaskTable />
+      </div>
+    </div>
+  );
+}
