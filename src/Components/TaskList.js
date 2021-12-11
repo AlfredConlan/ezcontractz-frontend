97bdc66c33cd672e5e28ab66f70e788a6aa773a6
Al Conlan <oconellan@gmail.com>
2021-12-11 14:23:44 -0600

    Working on SetUserName

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -2,12 +2,12 @@ import React from "react";
 // import Profile from "./Profile";
 // import BasicTable from './dataTable'
 import TaskTable from "./TasksTable";
-import setUserName from "./SetUserName";
+import SetUserName from "./SetUserName";
 
 export default function TaskList() {
   return (
     <container>
-      <setUserName />
+      <SetUserName />
       <container className="profileHouseSection">
         <h1 className="text-center p-4 text-primary">Your Tasks</h1>
       </container>
