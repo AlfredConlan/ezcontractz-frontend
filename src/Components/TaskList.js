886a5543761711d7657bfc3afdb8e6f3a748b872
Al Conlan <oconellan@gmail.com>
2021-12-11 13:57:15 -0600

    added SetUserName component

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Profile from "./Profile";
+// import Profile from "./Profile";
 // import BasicTable from './dataTable'
 import TaskTable from "./TasksTable";
+import setUserName from "./SetUserName";
 
 export default function TaskList() {
   return (
     <container>
+      <setUserName />
       <container className="profileHouseSection">
         <h1 className="text-center p-4 text-primary">Your Tasks</h1>
       </container>
