be5ea30782ca50b4aea965527502c0638ee06da8
Carmen <miss.cdv@gmail.com>
2021-12-14 21:02:50 -0500

    modaldropdown

--- src/Components/MDropDown.js
+++ src/Components/MDropDown.js
@@ -1,29 +1,53 @@
 // import React, { useState } from "react";
-// import { Row, Col, Form, Button, InputGroup, FormControl, Card, Modal, Dropdown} from "react-bootstrap";
+// import {Dropdown} from "react-bootstrap";
 
-// 
-//     res.status(200).send(allTaskss);
-//   });
-
-// function MDropDown() {
-  
- 
-//   return (
-//     <div>
-//     <Dropdown.Menu show>
-//     <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-//     <Dropdown.Divider />
-//     <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-//     <Dropdown.Divider />
-//     <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
-//     <Dropdown.Divider />
-//     <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-//     </Dropdown.Menu>
-      
-//     </div>
-//   )
-// }
-
-// export default MDropDown
-
+// export default function MDropDown() {
+//     const [tasks, setTasks] = useState([]); 
+//     const [assignedContractor,setAssignedContractor] = useState("")
+// //fetch to get task by user
+// const usertask =() => {
+//   fetch(`https://ezcontractz-backend.herokuapp.com/tasks/${localStorage.getItem("UserName")}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         if (data.error) {
+//           alert(data.error);
+//         } else {
+//           console.log(data);
+//           setTasks(data.task);
+//         }        
+//     })
+// //put to update the assigned contractor to the task   
+// const assignContractor = () => {
+// fetch(`https://ezcontractz-backend.herokuapp.com/tasks/${localStorage.getItem("UserName")}`,{
+//           method: "PUT",
+//           credentials: "include",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//         })
+//           .then((res) => res.json())
+//           .then((data) => {
+//             if (data.error) {
+//               alert(data.error);
+//             } else {
+//               console.log(data);
+//               setAssignedContractor(assignedContractor);
+//             }
+//           });
+//         }
+// return (
     
+//     <div className = "contractorGrid">
+//     <Dropdown.Menu show> {
+//         tasks.map((task, index) => { 
+//          return(   <div key={index}>           
+//             <Dropdown.Item eventKey="1">{task.title}</Dropdown.Item>
+//             <Dropdown.Divider /> 
+//             </div>    
+//          )          
+//         }         
+//         )
+//     }</Dropdown.Menu>
+//     </div>
+// )
+// }}
